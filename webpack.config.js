f586c3ba5f61518619e0c07d4581d903a79d0b1b
Valmir Almeida <almeidavalmir76@gmail.com>
2022-02-01 00:45:13 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+//==== Importa path ====
+const path = require("path");
+
+module.exports = {
+  // __dirname --> Pega o diretório onde criei o arquivo /pasta / arquivo
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
